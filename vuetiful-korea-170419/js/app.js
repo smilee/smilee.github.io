a470467a390b5dfb9269f024d355403180940577
smilee <ideas007@naver.com>
2017-04-15 08:29:22 +0900

    Implement a "attack" method
    The user will now be able to attack the monster and see whether he or
    she won or not

--- vuetiful-korea-170419/js/app.js
+++ vuetiful-korea-170419/js/app.js
@@ -12,6 +12,41 @@ new Vue({
       //initialize healthbars
       this.playerHealth = 100;
       this.monsterHealth = 100;
+    },
+    attack: function() {
+      //player attacks
+      var max = 10;
+      var min = 3;
+      var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.monsterHealth -= damage;
+
+      //check if the player won
+      if (this.monsterHealth <= 0) {
+        alert('You won!');
+        this.gameIsRunning = false;
+        return;
+      }
+
+      //monster attacks
+      max = 12;
+      min = 5;
+      damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.playerHealth -= damage;
+
+      //check if the monster won
+      if (this.playerHealth <= 0) {
+        alert('You lost!');
+        this.gameIsRunning = false;
+      }
+    },
+    specialAttack: function() {
+
+    },
+    heal: function() {
+
+    },
+    giveUp: function() {
+
     }
   }
 });
