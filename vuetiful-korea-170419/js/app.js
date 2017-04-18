d4cf17e7665915fe259fe19b12624a536ad44116
smilee <ideas007@naver.com>
2017-04-18 19:15:36 +0900

    Styling upgrade
    I hope this looks better

--- vuetiful-korea-170419/js/app.js
+++ vuetiful-korea-170419/js/app.js
@@ -82,6 +82,7 @@ new Vue({
     },
     checkWin: function() {
       if (this.monsterHealth <= 0) {
+        this.monsterHealth = 0;
         if (confirm('승리! 한번 더 하시겠습니까?')) {
           this.startGame();
         } else {
@@ -89,6 +90,7 @@ new Vue({
         }
         return true;
       } else if (this.playerHealth <= 0) {
+        this.playerHealth = 0;
         if (confirm('패배! 한번 더 하시겠습니까?')) {
           this.startGame();
         } else {
